1259af48145fb3d821f7aab37314c6e8f9921d98
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2026-03-12 20:14:34 -0600

    esPalindromo()

--- dosPunteros.js
+++ dosPunteros.js
@@ -31,3 +31,32 @@ function encontrarPar(arr, objetivo){
 let numeros = [2, 4, 7, 10, 11, 15, 20, 40, 80]
 let resultado = encontrarPar(numeros,25);
 console.log(resultado);
+
+// Ejercicio: Crear una funcion que confirme si un string esPalindromo(string)
+// anita lava la tina 
+// radar
+// palabra extra
+
+function esPalindromo(str) {
+    //Convertir todo a minusculas 
+    str = str.toLowerCase();
+
+    let izq = 0; 
+    let der = str.length -1; 
+
+    while (izq < der){
+        //Comparamos cada caracter del lado izq con el el caracter del lado der 
+        if(str[izq] !== str[der]){
+            return false;
+        }
+        //Si si coinciden movemos los punteros de izq hacia derecha
+        izq++
+        //El derecho retrocede hacia la izq
+        der--;
+    }
+    // Encontramos que si es Palindromo
+    return true;
+}
+
+let palabra = 'javascript';
+console.log(esPalindromo(palabra));
