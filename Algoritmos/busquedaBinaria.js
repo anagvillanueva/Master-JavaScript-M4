7b1886b43181dd36f63b381dbcb7299079e30ddd
Ana Villanueva <54164289+anagvillanueva@users.noreply.github.com>
2026-03-26 19:45:16 -0600

    Busqueda binaria con comentarios

--- Algoritmos/busquedaBinaria.js
+++ Algoritmos/busquedaBinaria.js
@@ -1,25 +1,30 @@
 function binarySearch(arr,target,low = 0, high = arr.length - 1){
+
+    console.log(`Buscando el numero ${target} entre los indices ${low} y ${high}`)
     // Caso base, donde ya no podamos buscar 
     if(low > high){
+        console.log('No encontrado')
         return -1; // No lo encontro
     }
 
     // Paso 1. Dividir 
     let mid = Math.floor((low + high) / 2);
+    console.log( `MID: ${mid}, Valor: ${arr[mid]}`)
 
     // Paso 2. Conquistar 
     if(arr[mid] === target){
+        console.log(`Encontrado en el indice ${mid}`)
         return mid; // Si encontramos ese valor
     }
 
     //Paso 3.Seguir diviendo 
     if(arr[mid] > target){
-        //Buscamos en la mitad izquierda 
+        console.log('Buscamos en la mitad izquierda')
         return binarySearch(arr, target, low, mid - 1);
     }else {
-        //Buscar en la mitad derecha 
+        console.log('Buscamos en la mitad derecha') 
         return binarySearch(arr, target, mid + 1, high)
     }
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 5));
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 9));
